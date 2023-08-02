20a5ec0c4c11279ad0ebf53ee1e17014624b33d1
pachong007 <ydtg19930330@gmail.com>
2023-08-02 17:25:15 +0800

    1

--- www/one-cartoon/app/Console/Commands/VerifyComic.php
+++ www/one-cartoon/app/Console/Commands/VerifyComic.php
@@ -61,12 +61,12 @@ class VerifyComic extends Command
         foreach ($DBs as $D) {
             $db = $D->name;
             config(["database.connections.mysql_${db}" => [
-                'driver'    => 'mysql',
-                'host'      => '107.148.191.71',
-                'database'  => $db,
-                'username'  => 'root',
-                'password'  => 'docker@6603',
-                'charset'   => 'utf8',
+                'driver' => 'mysql',
+                'host' => '107.148.191.71',
+                'database' => $db,
+                'username' => 'root',
+                'password' => 'docker@6603',
+                'charset' => 'utf8',
                 'collation' => 'utf8_unicode_ci'
             ]]);
 
@@ -77,98 +77,98 @@ class VerifyComic extends Command
                     SourceComic::where('id', $sourceComic->id)->update(['status' => 2, 'updated_at' => date('Y-m-d H:i:s')]);
                     continue;
                 }
-                $publish = Publish::where(['database'=>$db,'publish_id'=>$sourceComic->id])->first();
-                if(!$publish){
+                $publish = Publish::where(['database' => $db, 'publish_id' => $sourceComic->id])->first();
+                if (!$publish) {
                     $comic = new Comic();
                     $comic->setConnection("mysql_${db}");
                     $publishId = $comic->insertGetId([
-                       'name'=>$sourceComic->title,
-                        'yname'=>'',
-                        'pic'=>rtrim(env("IMG_DOMAIN"),"/")."/".$sourceComic->cover,
-                        'cid'=>mt_rand(1,4),
-                        'serialize'=>$sourceComic->is_finish == 1 ? '完结':'连载',
-                        'author'=>$sourceComic->author,
-                        'content'=>$sourceComic->description,
-                        'nums'=>$sourceComic->chapter_count,
-                        'score'=>mt_rand(1,9),
-                        'did'=>$sourceComic->id,
-                        'ly'=>'kk',
-                        'addtime'=>time(),
+                        'name' => $sourceComic->title,
+                        'yname' => '',
+                        'pic' => rtrim(env("IMG_DOMAIN"), "/") . "/" . $sourceComic->cover,
+                        'cid' => mt_rand(1, 4),
+                        'serialize' => $sourceComic->is_finish == 1 ? '完结' : '连载',
+                        'author' => $sourceComic->author,
+                        'content' => $sourceComic->description,
+                        'nums' => $sourceComic->chapter_count,
+                        'score' => mt_rand(1, 9),
+                        'did' => $sourceComic->id,
+                        'ly' => 'kk',
+                        'addtime' => time(),
                     ]);
                     $mct = new McType();
                     $mct->setConnection("mysql_${db}");
-                    $tag = $mct->where('fid',1)->inRandomOrder()->first();
-                    $tag2 = $mct->where('fid',2)->inRandomOrder()->first();
-                    $tag3 = $mct->where('fid',3)->inRandomOrder()->first();
-                    $tag4 = $mct->where('fid',4)->inRandomOrder()->first();
-                    $tag5 = $mct->where('fid',5)->inRandomOrder()->first();
+                    $tag = $mct->where('fid', 1)->inRandomOrder()->first();
+                    $tag2 = $mct->where('fid', 2)->inRandomOrder()->first();
+                    $tag3 = $mct->where('fid', 3)->inRandomOrder()->first();
+                    $tag4 = $mct->where('fid', 4)->inRandomOrder()->first();
+                    $tag5 = $mct->where('fid', 5)->inRandomOrder()->first();
                     $typeInsert = [
-                        ['mid'=>$publishId,'tid'=>$tag->id],
-                        ['mid'=>$publishId,'tid'=>$tag2->id],
-                        ['mid'=>$publishId,'tid'=>$tag3->id],
-                        ['mid'=>$publishId,'tid'=>$tag4->id],
-                        ['mid'=>$publishId,'tid'=>$tag5->id],
+                        ['mid' => $publishId, 'tid' => $tag->id],
+                        ['mid' => $publishId, 'tid' => $tag2->id],
+                        ['mid' => $publishId, 'tid' => $tag3->id],
+                        ['mid' => $publishId, 'tid' => $tag4->id],
+                        ['mid' => $publishId, 'tid' => $tag5->id],
                     ];
                     $ct = new ComicType();
                     $ct->setConnection("mysql_${db}");
                     $ct->insert($typeInsert);
 
                     Publish::insert([
-                        'comic_id'=>$sourceComic->id,
-                        'chapter_id'=>json_encode([]),
-                        'source'=>$sourceComic->source,
-                        'database'=>$db,
-                        'publish_id'=>$publishId,
-                        'publish_chapter_id'=>json_encode([])
+                        'comic_id' => $sourceComic->id,
+                        'chapter_id' => json_encode([]),
+                        'source' => $sourceComic->source,
+                        'database' => $db,
+                        'publish_id' => $publishId,
+                        'publish_chapter_id' => json_encode([])
                     ]);
-                    $publish = Publish::where(['database'=>$db,'publish_id'=>$sourceComic->id])->first();
+                    $publish = Publish::where(['database' => $db, 'publish_id' => $sourceComic->id])->first();
                 }
-                if($publish){
-                    if($chapterDone <= count($publish->publish_chapter_id)){
-                        continue;
-                    }
-                    $this->insertChapter($db,$sourceComic->id,$publish->publish_id,$publish->chapter_id);
+                var_dump($chapterDone,count($publish->publish_chapter_id),'line1');
+                if ($chapterDone <= count($publish->publish_chapter_id)) {
                     continue;
                 }
+                $this->insertChapter($db, $sourceComic->id, $publish->publish_id, $publish->chapter_id);
+                continue;
             }
         }
     }
 
-    private function insertChapter($db,$comicId,$mid,$chapterIds)
+    private function insertChapter($db, $comicId, $mid, $chapterIds)
     {
         $chapterLimit = $this->chapterLimit;
-        if(!empty($chapterIds)) {
-            $chapters = SourceChapter::where('comic_id',$comicId)->whereNotIn('id', $chapterIds)
+        if (!empty($chapterIds)) {
+            $chapters = SourceChapter::where('comic_id', $comicId)->whereNotIn('id', $chapterIds)
                 ->get()->toArray();
-        }else{
-            $chapters = SourceChapter::where('comic_id',$comicId)->get()->toArray();
+        } else {
+            $chapters = SourceChapter::where('comic_id', $comicId)->get()->toArray();
         }
-        foreach ($chapters as $chapter){
-            if($chapterLimit<0)break;
-            if(!empty($chapter['image']) && $chapter['image']['state'] == 1){
+        var_dump($chapters,$chapterIds,'line2');
+        foreach ($chapters as $chapter) {
+            if ($chapterLimit < 0) break;
+            if (!empty($chapter['image']) && $chapter['image']['state'] == 1) {
                 $images = $chapter['image']['images'];
                 $cha = new Chapter();
                 $cha->setConnection("mysql_${db}");
                 $cid = $cha->insertGetId([
-                    'mid'=>$mid,
-                    'xid'=>$chapter['sort'],
-                    'name'=>$chapter['title'],
-                    'jxurl'=>$chapter['source_url'],
-                    'pnum'=>count($images),
-                    'addtime'=>time(),
+                    'mid' => $mid,
+                    'xid' => $chapter['sort'],
+                    'name' => $chapter['title'],
+                    'jxurl' => $chapter['source_url'],
+                    'pnum' => count($images),
+                    'addtime' => time(),
                 ]);
 
                 $insertImages = [];
-                foreach ($images as $k=>$image){
+                foreach ($images as $k => $image) {
                     $insertImages[] = [
-                      'cid'=>$cid,
-                      'mid'=>$mid,
-                      'img'=>rtrim(env("IMG_DOMAIN"),"/")."/".$image,
-                      'xid'=>$k,
-                      'md5'=>''
+                        'cid' => $cid,
+                        'mid' => $mid,
+                        'img' => rtrim(env("IMG_DOMAIN"), "/") . "/" . $image,
+                        'xid' => $k,
+                        'md5' => ''
                     ];
                 }
-                if(!empty($insertImages)) {
+                if (!empty($insertImages)) {
                     $img = new Image();
                     $img->setConnection("mysql_${db}");
                     $img->insert($insertImages);
