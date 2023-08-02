c365427b2da7875be931c145b278d3a55a53dd9c
pachong007 <ydtg19930330@gmail.com>
2023-08-02 11:31:59 +0800

    1

--- www/one-cartoon/app/Models/Comic.php
+++ www/one-cartoon/app/Models/Comic.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comic extends Model
 {
+    public $timestamps = false;
     protected $connection = 'mysql';
     protected $table = 'mc_comic';
 }
