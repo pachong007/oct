cf2511517d0a18eea0875b353158aaad23021335
pachong007 <ydtg19930330@gmail.com>
2023-08-03 14:56:40 +0800

    1

--- www/craw/robot/colly.go
+++ www/craw/robot/colly.go
@@ -28,6 +28,7 @@ func GetColly() *colly.Collector {
 }
 
 func GetProxy() string {
+	api := "https://dvapi.doveproxy.net/cmapi.php?rq=distribute&user=yipinbao6688&token=eUkxbHhCSFZFcit1TS9XRWdxVy9mUT09&auth=0&geo=all&city=all&agreement=1&timeout=35&num=10&rtype=0"
 	var saveData []string
 	proxy := ""
 	cache := "proxy:" + config.Spe.SourceUrl
@@ -44,7 +45,7 @@ func GetProxy() string {
 		}
 	}
 	for {
-		content, code, _ := tools.HttpRequest("https://dvapi.doveproxy.net/cmapi.php?rq=distribute&user=carter&token=ZjNKNFZlSHRQNmlhY1R0MCtpY0tKQT09&auth=0&geo=PH&city=208622&agreement=1&timeout=25&num=10&rtype=0",
+		content, code, _ := tools.HttpRequest(api,
 			"GET", "", map[string]string{}, []*http.Cookie{})
 		if code == 200 {
 			res := gjson.Parse(content)
