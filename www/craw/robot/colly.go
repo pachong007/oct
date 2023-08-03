aaad0ca9a25695de8ebc8f552974ade922fa47e2
pachong007 <ydtg19930330@gmail.com>
2023-08-03 13:36:04 +0800

    1

--- www/craw/robot/colly.go
+++ www/craw/robot/colly.go
@@ -4,9 +4,11 @@ import (
 	"comics/tools"
 	"comics/tools/config"
 	"comics/tools/rd"
+	"encoding/json"
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/extensions"
 	"github.com/tidwall/gjson"
+	"math/rand"
 	"net/http"
 	"time"
 )
@@ -25,22 +27,36 @@ func GetColly() *colly.Collector {
 }
 
 func GetProxy() string {
+	var saveData []string
 	proxy := ""
 	cache := "proxy:" + config.Spe.SourceUrl
 	cacheProxy := rd.Get(cache)
 	if cacheProxy != "" {
-		return cacheProxy
+		err := json.Unmarshal([]byte(cacheProxy), &saveData)
+		if err != nil {
+			panic(err)
+		}
+		if len(saveData) > 0 {
+			randomIndex := rand.Intn(len(saveData))
+			return saveData[randomIndex]
+		}
 	}
 	for {
-		content, code, _ := tools.HttpRequest("https://dvapi.doveproxy.net/cmapi.php?rq=distribute&user=yipinbao6688&token=eUkxbHhCSFZFcit1TS9XRWdxVy9mUT09&auth=0&geo=NZ&city=361574&agreement=1&timeout=20&num=1&rtype=0",
+		content, code, _ := tools.HttpRequest("https://dvapi.doveproxy.net/cmapi.php?rq=distribute&user=carter&token=ZjNKNFZlSHRQNmlhY1R0MCtpY0tKQT09&auth=1&geo=all&city=all&agreement=1&timeout=25&num=10&rtype=0",
 			"GET", "", map[string]string{}, []*http.Cookie{})
 		if code == 200 {
 			res := gjson.Parse(content)
-			proxy = "http://" + res.Get("data").Get("ip").String() + ":" + res.Get("data").Get("port").String()
-			rd.Set(cache, proxy, time.Minute*15)
+			for _, d := range res.Get("data").Array() {
+				saveData = append(saveData, "http://"+d.Get("data").Get("ip").String()+":"+d.Get("data").Get("port").String())
+			}
+			jsonData, err := json.Marshal(saveData)
+			if err != nil {
+				panic(err)
+			}
+			rd.Set(cache, string(jsonData), time.Minute*20)
 			break
 		}
-		if code == 409 {
+		if code != 200 {
 			t := time.NewTicker(time.Second * 30)
 			<-t.C
 		}
